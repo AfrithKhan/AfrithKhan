35b94dbe3b75a54a3b06242116301e9a427311a0
manoj_softsuave <manoj.rajendran@softsuave.com>
2022-05-27 16:39:22 +0530

    app spec file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 
 app.get('/', (req, res)=> {
-res.send('Hello Afrith!');
+res.send('Hello Afrith! I am changed');
 });
 app.get('/manoj', (req, res)=> {
     res.send('Hello Manoj!');
